998a2497b86bd138f5d17a98a510ebab39809d03
Slyazee <31852641+Slyazee@users.noreply.github.com>
2017-10-04 14:05:55 +0200

    js.1

---
+++ index.js
@@ -0,0 +1,8 @@
+var SEK, EUR, USD, DKK;
+function init()
+{
+    SEK = document.getElementById("SEK");
+    EUR = document.getElementById("EUR");
+    USD = document.getElementById("USD");
+    DKK = document.getElementById("DKK");
+	]
