8e9544d309068a19634b21c3a66ae25a858f3a62
Slyazee <31852641+Slyazee@users.noreply.github.com>
2017-10-04 14:06:41 +0200

    js.2

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 var SEK, EUR, USD, DKK;
 function init()
 {
-    SEK = document.getElementById("SEK");
-    EUR = document.getElementById("EUR");
-    USD = document.getElementById("USD");
-    DKK = document.getElementById("DKK");
+    sek = document.getElementById("SEK");
+    eur = document.getElementById("EUR");
+    usd = document.getElementById("USD");
+    dkk = document.getElementById("DKK");
 	]
+function sekfunc()
+{
+    eur.value = parseFloat(0.value) * 0.10440593;
+    usd.value = parseFloat(0.value) * 0.12250848;
+    dkk.value = parseFloat(0.value) * 0.77705089;
+}
+function usdfunc()
+{
+    sek.value = parseFloat(1.value) * 8.12090402;
+    eur.value = parseFloat(1.value) * 8,497904303;
+    dkk.value = parseFloat(1.value) * 1.38814;
+}
+function eurfunc()
+{
+    sek.value = parseFloat(2.value) * 9.55732952;
+    usd.value = parseFloat(2.value) * 1.17688;
+    dkk.value = parseFloat(2.value) * 7.44248403;
+}
+function dkkfunc()
+{
+    sek.value = parseFloat(3.value) * 1.28415855;
+    eur.value = parseFloat(3.value) * 0.134363741;
+    usd.value = parseFloat(3.value) * 0.15813;  
+}
